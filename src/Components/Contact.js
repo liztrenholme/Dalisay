17ca6e1859f43b4693d36e9bde550677c039e0a9
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-08-02 23:07:47 -0400

    style tweaks to contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,10 +11,13 @@ class Contact extends Component {
                         </div>
                         <div className="col-md-6">
                             <form action="https://formspree.io/liztrenholme@gmail.com" method="POST">
-                                <input type="text" required name="fname" placeholder="First name" />
-                                <input type="text" required name="lname" placeholder="Last name" />
-                                <input type="email" required name="_replyto" placeholder="Your email address" />
-                                <textarea required rows="10" cols="40" name="message" placeholder="Message..." width="50%" height="50%"></textarea>
+                                <input type="text" required name="fname" />
+                                <label htmlFor="fname">First Name*</label>
+                                <input type="text" required name="lname" />
+                                <label htmlFor="lname">Last Name*</label>
+                                <input type="email" required name="_replyto" />
+                                <label htmlFor="email">Your email address*</label>
+                                <textarea required rows="10" cols="40" name="message" placeholder="Message...*" width="50%" height="50%"></textarea>
                                 <input type="submit" className='btn-secondary' value="Send" />
                             </form>
                         </div>
